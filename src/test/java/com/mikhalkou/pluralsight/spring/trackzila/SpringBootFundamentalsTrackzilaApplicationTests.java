5bee8996e859f379f9a154085eabd7d542966289
Maksim Mikhalkou <maksim@mikhalkou.com>
2021-05-18 00:01:55 +0200

    Init project

---
+++ src/test/java/com/mikhalkou/pluralsight/spring/trackzila/SpringBootFundamentalsTrackzilaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikhalkou.pluralsight.spring.trackzila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFundamentalsTrackzilaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
